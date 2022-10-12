ea95de78d8caca44d82682332f20fe7b7643dc88
Fred Glass <glasshcs@gmail.com>
2022-10-12 20:50:36 +0100

    Add initial content pages

---
+++ src/page/ContentPage.svelte
@@ -0,0 +1,38 @@
+<script>
+    export let title;
+    export let image;
+</script>
+
+<svelte:head>
+    <title>fred.glass · {title}</title>
+</svelte:head>
+
+<div class="container">
+    {#if image}
+        <div class="mt-4">
+            <img src={image} alt="Image" width="200">
+        </div>
+    {/if}
+    <h1 class="text-light"># {title}</h1>
+    <p class="lead text-secondary">
+        <slot />
+    </p>
+</div>
+
+<style>
+    .container {
+        max-width: 688px;
+    }
+    h1 {
+        padding-bottom: 15px;
+        border-bottom: 1px solid rgba(84, 84, 84, 0.48);
+    }
+    p {
+        text-align: justify;
+    }
+    img {
+        margin-bottom: 30px;
+        border: 1px solid #343a40;
+        border-radius: .25rem;
+    }
+</style>
--- src/page/MiscPage.svelte
+++ src/page/MiscPage.svelte
@@ -1,5 +1,6 @@
 <script>
-    import RepoCard from "svelte-repo-card";
+    // import RepoCard from "svelte-repo-card";
+    import ContentPage from "./ContentPage.svelte";
 
     const theme = {
         text: "#8b949e",
@@ -13,19 +14,56 @@
     <title>fred.glass · Misc</title>
 </svelte:head>
 
-<div class="container">
-    <h1 class="text-light">Miscellaneous</h1>
-    <p class="lead text-secondary">
-        A collection of small, experimental projects created for fun or to solve a problem
-    </p>
-    <div class="cards mt-4">
-        <RepoCard slug="fglass/readunwise" theme={theme} />
-        <RepoCard slug="fglass/obsidx" theme={theme} />
-        <RepoCard slug="fglass/advent-of-code" theme={theme} />
-    </div>
-</div>
+<ContentPage title="Miscellaneous">
+    <h2 >## Projects</h2>
+    <ul>
+        <li>readunwise</li>
+        <li>obsidx</li>
+        <li>advent-of-code</li>
+    </ul>
+    <h2>## Reading</h2>
+    2022
+    <ul>
+        <li>Pragmatic Programmer<span>⭐️⭐️</span>️️</li>
+        <li>Atomic Habits<span>⭐️⭐️⭐️⭐️</span></li>
+        <li>Finish: Give Yourself the Gift of Done<span>️⭐️</span>️️</li>
+        <li>Clean Code<span>⭐️⭐️⭐️⭐️</span></li>
+        <li>Designing Data-Intensive Applications<span>⭐️⭐️⭐️⭐️⭐️</span></li>
+        <li>How Life Imitates Chess<span>⭐️⭐️</span></li>
+        <li>Cracking the Coding Interview<span>⭐️⭐️⭐️</span></li>
+        <li>Architecture Patterns with Python<span>⭐️⭐️⭐️</span></li>
+        <li>How to Solve It<span>⭐️⭐️⭐️</span></li>
+    </ul>
+    2021
+        <ul>
+        <li>Pragmatic Programmer<span>⭐️⭐️</span>️️</li>
+        <li>Atomic Habits<span>⭐️⭐️⭐️⭐️</span></li>
+        <li>Finish: Give Yourself the Gift of Done<span>️⭐️</span>️️</li>
+        <li>Clean Code<span>⭐️⭐️⭐️⭐️</span></li>
+        <li>Designing Data-Intensive Applications<span>⭐️⭐️⭐️⭐️⭐️</span></li>
+        <li>How Life Imitates Chess<span>⭐️⭐️</span></li>
+        <li>Cracking the Coding Interview<span>⭐️⭐️⭐️</span></li>
+        <li>Architecture Patterns with Python<span>⭐️⭐️⭐️</span></li>
+        <li>How to Solve It<span>⭐️⭐️⭐️</span></li>
+    </ul>
+</ContentPage>
+
+<!--<div class="container">-->
+<!--    <h1 class="text-light">Miscellaneous</h1>-->
+<!--    <p class="lead text-secondary">-->
+<!--        A collection of small, experimental projects created for fun or to solve a problem-->
+<!--    </p>-->
+<!--    <div class="cards mt-4">-->
+<!--        <RepoCard slug="fglass/readunwise" theme={theme} />-->
+<!--        <RepoCard slug="fglass/obsidx" theme={theme} />-->
+<!--        <RepoCard slug="fglass/advent-of-code" theme={theme} />-->
+<!--    </div>-->
+<!--</div>-->
 
 <style>
+    span {
+        float: right;
+    }
     .cards {
         display: grid;
         grid-template-columns: 1fr 1fr;
